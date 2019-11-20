46a3f7747a2e6652b66882bc3f6a1807456ab1c4
rgmprabodha <rgmprabodha@gmail.com>
2019-11-20 10:28:00 +0100

    Initial commit

---
+++ src/test/java/com/emse/FairCorp/FairCorpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emse.FairCorp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairCorpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
