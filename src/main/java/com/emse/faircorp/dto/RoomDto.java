17489c86e2c371073a1930f752c18203d3537122
rgmprabodha <rgmprabodha@gmail.com>
2020-01-01 13:27:51 +0100

    Add lights to RoomDto

--- src/main/java/com/emse/faircorp/dto/RoomDto.java
+++ src/main/java/com/emse/faircorp/dto/RoomDto.java
@@ -4,18 +4,30 @@ import com.emse.faircorp.model.Light;
 import com.emse.faircorp.model.Room;
 import com.emse.faircorp.model.Status;
 
+import java.util.Set;
+
 public class RoomDto {
     private Long id;
     private String name;
     private Integer floor;
+    private Set<Light> lights;
 
     public RoomDto() {
+    }
+
+    public Set<Light> getLights() {
+        return lights;
+    }
+
+    public void setLights(Set<Light> lights) {
+        this.lights = lights;
     }
 
     public RoomDto(Room room) {
         this.id = room.getId();
         this.name = room.getName();
         this.floor = room.getFloor();
+        this.lights = room.getLights();
     }
 
     public Long getId() {
